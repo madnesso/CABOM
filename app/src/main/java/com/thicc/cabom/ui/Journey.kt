ab4534817853abdbe4a170ae19544d366f0da083
madnesso <deadsasuke@gmail.com>
2020-07-22 19:11:16 +0200

    no

--- app/src/main/java/com/thicc/cabom/ui/Journey.kt
+++ app/src/main/java/com/thicc/cabom/ui/Journey.kt
@@ -1,7 +1,8 @@
-package com.thicc.cabom
+package com.thicc.cabom.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thicc.cabom.R
 
 class Journey : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
