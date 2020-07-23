08fb5e8bae0879c68adacd9b052bf31c5e59a4eb
Caroline Ehab <caroline.ehab.1997@gmail.com>
2020-07-23 05:55:08 +0200

    Quiz without Pop Up

---
+++ app/src/main/java/com/thicc/cabom/ui/Questions.kt
@@ -0,0 +1,12 @@
+package com.thicc.cabom.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thicc.cabom.R
+
+class Questions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
