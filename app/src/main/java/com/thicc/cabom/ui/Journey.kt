a9e89853660ef75675ecfe6d6a15115c992ab27c
madnesso <deadsasuke@gmail.com>
2020-07-28 07:25:10 +0200

    added new ProgressNavigationBar

--- app/src/main/java/com/thicc/cabom/ui/Journey.kt
+++ app/src/main/java/com/thicc/cabom/ui/Journey.kt
@@ -1,12 +1,33 @@
 package com.thicc.cabom.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.library.NavigationBar
+import com.library.NvTab
 import com.thicc.cabom.R
+import kotlinx.android.synthetic.main.activity_journey.*
 
-class Journey : AppCompatActivity() {
+
+class Journey : AppCompatActivity(), NavigationBar.OnTabSelected, NavigationBar.OnTabClick {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journey)
+        val bar = navBar as NavigationBar
+        bar.onTabSelected = this
+        bar.onTabClick = this
+        bar.tabCount = 5
+        bar.animateView(3000)
+        val pos = 1
+        bar.currentPosition = pos
+
+    }
+
+    override fun onTabSelected(touchPosition: Int, prev: NvTab?, nvTab: NvTab?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTabClick(touchPosition: Int, prev: NvTab?, nvTab: NvTab?) {
+        TODO("Not yet implemented")
     }
 }
